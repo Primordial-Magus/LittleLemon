e0b07cfe741f47cc108b8cc32cde45dbf98f873a
Eric Tran <erictran0007@gmail.com>
2024-01-24 18:28:33 +1100

    Added printed strings to models

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,25 +1,39 @@
 from django.db import models
-# from django.core.validators import MaxValueValidator, MinLengthValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
+from decimal import Decimal
 
 
 # Create your models here.
 class Booking(models.Model):
-    ID = models.IntegerField(
-        primary_key = True
+    ID = models.PositiveIntegerField(
+        primary_key = True,
+        unique = True
     )
     Name = models.CharField(max_length=255)
     No_of_guests = models.IntegerField(
     )
     BookingDate = models.DateTimeField()
 
+    def __str__(self):
+        return str(self.ID) + " " +  str(self.Name)
+
+
+
 
 class Menu(models.Model):
-    ID = models.IntegerField(
-        primary_key = True
+    ID = models.PositiveIntegerField(
+        primary_key = True,
+        unique = True
     )
     Title = models.CharField(max_length=255)
-    Price = models.DecimalField(max_digits=10, decimal_places=2)
-    Inventory = models.IntegerField()
+    Price = models.DecimalField(max_digits=10, decimal_places=2, validators=
+                                [
+                                    MinValueValidator(Decimal('0.01'))
+                                ])
+    Inventory = models.PositiveIntegerField()
+
+    def __str__(self):
+        return str(self.ID) + " " +  str(self.Title)
 
 
 
